367ca2573bfbae126db45e71398f1a9522d55100
EC2 Default User <ec2-user@ip-172-31-18-120.ap-southeast-2.compute.internal>
2024-05-11 10:44:01 +0000

    deploy

--- db/migrate/20240411122423_add_post_image_id_to_post_comment.rb
+++
@@ -1,6 +0,0 @@
-class AddPostImageIdToPostComment < ActiveRecord::Migration[6.1]
-  def change
-    add_column :post_comments, :post_image_id, :integer
-    add_column :post_comments, :user_id, :integer
-  end
-end
