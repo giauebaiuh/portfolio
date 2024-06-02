2de475e07d8e57027b9d21a88d00f69122c7f430
EC2 Default User <ec2-user@ip-172-31-18-120.ap-southeast-2.compute.internal>
2024-06-02 11:45:06 +0000

    reset

--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,12 @@ Rails.application.routes.draw do
   devise_for :admin, skip: [:registrations, :password], controllers: {
     sessions: 'admin/sessions'
   }
-  
+
   namespace :admin do
     get 'dashboards', to: 'dashboards#index'
-    resources :users, only: [:destroy]
+    resources :users, only: [:destroy,]
   end
-  
+
   scope module: :public do
     devise_for :users
     # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
